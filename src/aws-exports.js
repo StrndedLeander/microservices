4598f166c4e42940e8ad129db22816fbad4a813f
StrndedLeander <strnded.vandiepen@gmail.com>
2019-02-04 20:21:33 +0100

    added users api with endpoint to lambda function adding user to ddb

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -8,7 +8,14 @@ const awsmobile =  {
     "aws_cognito_identity_pool_id": "eu-central-1:a299930b-0540-4ae5-ae2e-9895b7b0e20f",
     "aws_cognito_region": "eu-central-1",
     "aws_user_pools_id": "eu-central-1_9fDkk6Zv7",
-    "aws_user_pools_web_client_id": "22dr3cigikc9p1odc4245lu2kc"
+    "aws_user_pools_web_client_id": "22dr3cigikc9p1odc4245lu2kc",
+    "aws_cloud_logic_custom": [
+        {
+            "name": "usersAuthAPI",
+            "endpoint": "https://x0e8bg0lvi.execute-api.eu-central-1.amazonaws.com/Prod",
+            "region": "eu-central-1"
+        }
+    ]
 };
 
 
